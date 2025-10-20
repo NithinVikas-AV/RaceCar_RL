6b49afd1e9056dc3a943868caf84bfe7c2b2d93d
Nithin Vikas <nithinvikas8389@gmail.com>
2025-10-20 22:18:08 +0530

    feat: Code for training and testing the model.

--- Train_PPO_Agent.py
+++ Train_PPO_Agent.py
@@ -1,8 +1,9 @@
 import os
+import hashlib
 import gymnasium as gym
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import SubprocVecEnv
 
 def make_env():
     def _init():
@@ -14,11 +15,18 @@ def make_env():
 num_envs = 8   # adjust based on your CPU
 env = SubprocVecEnv([make_env() for _ in range(num_envs)])
 
-log_path = os.path.join("Training", "Logs")
+log_path = os.path.join('Learnings', 'Training', 'Logs')
 
 model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=log_path)
 
-ppo_path = os.path.join('Training', 'Saved Models', 'PPO_491k_CarRacing_Model')
+ppo_path = os.path.join('Learnings', 'Training', 'Saved Models', 'PPO_491k_CarRacing_Model')
 
 model.learn(total_timesteps=491520)
 model.save(ppo_path)
+
+# Generate a SHA-256 hash for model integrity verification
+with open(ppo_path + ".zip", "rb") as f:
+    file_bytes = f.read()
+    hash_value = hashlib.sha256(file_bytes).hexdigest()
+    with open('hash_value.txt', 'w') as hash_file:
+        hash_file.write(hash_value)
